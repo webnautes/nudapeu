a3a1ff77950e25b92bc740fbe9eb526129cef397
webnautes <webnautes@nate.com>
2019-04-23 09:51:02 +0900

    Create opencv-python-0031.py

---
+++ opencv-python-0031.py
@@ -0,0 +1,21 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret,img_color = cap.read()
+
+    if ret == False:
+        continue
+
+    img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("Color", img_color)
+    cv2.imshow("Gray", img_gray)
+
+    if cv2.waitKey(1)&0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
