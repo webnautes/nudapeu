18832b6d93909344ee10df73d9fcd0915ee51d43
webnautes <webnautes@nate.com>
2019-04-23 09:41:27 +0900

    Create opencv-python-002.py

---
+++ opencv-python-002.py
@@ -0,0 +1,22 @@
+
+import cv2
+
+
+img_color = cv2.imread('Billiard.jpg', cv2.IMREAD_COLOR)
+
+cv2.namedWindow('Show Image')
+cv2.imshow('Show Image', img_color)
+
+cv2.waitKey(0)
+
+
+img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Show Image', img_gray)
+cv2.waitKey(0)
+
+
+cv2.imwrite('savedimage.jpg', img_gray)
+
+
+cv2.destroyAllWindows()
